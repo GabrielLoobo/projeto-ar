45c8d74948e408e7022c5286212f7841b821d71b
Gabriel Lobo <gabriellobodurante@gmail.com>
2022-11-06 15:13:57 -0300

    cria método pra seleção de mapa por meio de programa

---
+++ Assets/Scripts/BoardConfiguration.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardConfiguration : MonoBehaviour
+{
+    public GameObject board_prefab;
+
+    public string selectedBoardName;
+    
+    private GameObject plane;
+
+    private string BOARD_ASSETS_FOLDER;
+    private Object[] materials;
+    private List<string> materialNames;
+
+    void Awake()
+    {
+        selectedBoardName = "";
+        BOARD_ASSETS_FOLDER = "Materials/Boards/";
+
+        materials  = Resources.LoadAll(BOARD_ASSETS_FOLDER, typeof(Material));
+        materialNames = new List<string>();
+        foreach (Material mat in materials)
+        {
+            materialNames.Add(mat.name);
+        }
+
+        setBoardPrefabMap("map2"); //   TODO: Isso está aqui como placeholder. Na prática, isso tem que ser chamado pelo componente de UI que seleciona o mapa
+    }
+
+    public  Material findMaterialByName(string matName)
+    {
+        // Provavelmente tem uma forma mais inteligente de fazer isso mas eu n�o sei C#
+
+        foreach(Material mat in materials)
+        {
+            if(mat.name == matName)
+            {
+                return mat;
+            }
+        }
+
+        return null;
+
+    }
+
+    private bool isValidMapName(string mapName)
+    {
+        foreach (string matName in materialNames)
+        {
+            Debug.Log("matName: " + matName + ", mapName: " + mapName);
+            if (matName == mapName)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void setBoardPrefabMap(string mapName)
+    {
+        Debug.Log("Setting board prefab");
+        if (isValidMapName(mapName))
+        {
+            selectedBoardName = mapName;
+        }
+    }
+
+}
--- Assets/Scripts/BoardImageTrackerController.cs
+++ Assets/Scripts/BoardImageTrackerController.cs
@@ -8,19 +8,13 @@ using UnityEngine.UI;
 [RequireComponent(typeof(ARTrackedImageManager))]
 public class BoardImageTrackerController : MonoBehaviour
 {
-    [SerializeField]
-    //private GameObject[] placeblePrefab;
-
-    //private Dictionary<string, GameObject> spawnedPrefabs = new Dictionary<string, GameObject>();
+    public BoardConfiguration boardConfiguration;
     private ARTrackedImageManager trackedImageManager;
 
     public GameObject board_prefab;
     private Button accept;
-    //public GameObject refuse;
 
     private GameObject i_board_prefab;
-    //private GameObject i_accept;
-    //private GameObject i_refuse;
 
     private bool isBoardPlaced;
 
@@ -31,6 +25,7 @@ public class BoardImageTrackerController : MonoBehaviour
         isBoardPlaced = true;
         accept.onClick.RemoveAllListeners();
         accept.gameObject.SetActive(false);
+        boardConfiguration.setBoardPrefabMap("map1");
     }
 
     private void Awake()
@@ -43,16 +38,7 @@ public class BoardImageTrackerController : MonoBehaviour
         accept = GameObject.Find("Confirm-UI").GetComponent<Button>();
         accept.onClick.AddListener(onConfirmClick);
 
-
-
         trackedImageManager.trackedImagesChanged += ImageChanged; // TODO: Talvez dê merda deixar isso aqui em vez do onEnable, confirmar depois
-
-        //foreach (GameObject prefab in placeblePrefab)
-        //{
-        //    GameObject newPrefab = Instantiate(prefab, Vector3.zero, Quaternion.identity);
-        //    newPrefab.name = prefab.name;
-        //    spawnedPrefabs.Add(prefab.name, newPrefab);
-        //}
     }
 
     void onEnable()
@@ -78,11 +64,6 @@ public class BoardImageTrackerController : MonoBehaviour
         {
             UpdateImage(trackedImage);
         }
-        //foreach (ARTrackedImage trackedImage in eventArgs.removed)
-        //{
-        //    i_board_prefab
-        //    //spawnedPrefabs[trackedImage.name].SetActive(false);
-        //}
     }
 
     private void UpdateImage(ARTrackedImage trackedImage)
@@ -94,27 +75,14 @@ public class BoardImageTrackerController : MonoBehaviour
             Vector3 position = trackedImage.transform.position;
             Quaternion rotation = trackedImage.transform.rotation;
 
+
             GameObject prefab = i_board_prefab;
-                prefab.transform.position = position;
-                prefab.transform.rotation = rotation;
-                prefab.SetActive(true);
-            }
 
-
-        //string name = trackedImage.referenceImage.name;
-        //Vector3 position = trackedImage.transform.position;
-
-        //GameObject prefab = spawnedPrefabs[name];
-        //prefab.transform.position = position;
-        //prefab.SetActive(true);
-
-        //foreach(GameObject go in spawnedPrefabs.Values)
-        //{
-        //    if(go.name != name)
-        //    {
-        //        go.SetActive(false);
-        //    }
-        //}
+            i_board_prefab.transform.GetChild(0).gameObject.GetComponent<Renderer>().material = boardConfiguration.findMaterialByName(boardConfiguration.selectedBoardName);
+            prefab.transform.position = position;
+            prefab.transform.rotation = rotation;
+            prefab.SetActive(true);
+        }
 
     }
 }
